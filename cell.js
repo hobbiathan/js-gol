de3f3f180ee7deb853ebb639277b27bcf48b42f2
hobbiathan <hobbiathan@gmail.com>
2022-10-14 22:09:03 -0600

    uhm uhh that uh uhm

--- cell.js
+++ cell.js
@@ -1,3 +1,30 @@
-function Cell() {
-    this.alive = false;
+function Cell(x, y, w) {
+
+    // Cell coords
+    this.x = x;
+    this.y = y;
+
+    // Cell width
+    this.w = w;
+
+    // Random cells will be dead and alive on load
+    if (random(1) < 0.31417) {
+        this.alive = true;
+    } else {
+        this.alive = false;
+    }
+}
+
+Cell.prototype.show = function() {
+    stroke(0);
+
+    if (this.alive) {
+        fill(255);
+    } else {
+        fill(0);
+    }
+    // Make square
+    rect(this.x, this.y, this.w, this.w)
+
+
 }
