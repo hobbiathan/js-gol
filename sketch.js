bf90fccb4bc4b341594c80e72f188bdeb4d19e2f
hobbiathan <hobbiathan@gmail.com>
2022-10-14 21:35:32 -0600

    rename runner.js -> sketch.js

---
+++ sketch.js
@@ -0,0 +1,38 @@
+// runner.js
+
+function makeGrid(columns, rows) {
+    // Array of arrays, cell position accessible via grid[x][y]
+    var arr = new Array(cols);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+var grid;
+var cols = 10;
+var rows = 10;
+
+
+function setup() {
+    // Setup canvas size to render
+    createCanvas(200, 200);
+    grid = makeGrid(cols, rows);
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            // Initialize all positions as new cell
+            grid[i][j] = new Cell(); 
+        }
+    }
+}  
+
+function draw() {
+    background(0);
+
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            grid[i][j].show();
+        }
+    }
+}
